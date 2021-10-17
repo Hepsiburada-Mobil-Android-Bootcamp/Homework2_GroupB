9ade6e11ca7001f12460abf3fa5a5f6975c13932
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-10-17 23:01:47 +0300

    Create Architecture

--- app/src/main/java/com/noor/homework2_groupb/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/noor/homework2_groupb/presentation/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.noor.homework2_groupb
+package com.noor.homework2_groupb.presentation.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.noor.homework2_groupb.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/noor/homework2_groupb/presentation/view/fragment/SplashFragment.kt
@@ -0,0 +1,7 @@
+package com.noor.homework2_groupb.presentation.view.fragment
+
+import androidx.fragment.app.Fragment
+
+class SplashFragment : Fragment() {
+
+}
