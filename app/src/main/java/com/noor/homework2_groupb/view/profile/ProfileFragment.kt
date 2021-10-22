4bc1bd523f6cef34f4d1c934c886d01ae3f43373
Serhad Mert <1301smert@gmail.com>
2021-10-22 16:57:32 +0300

    Login/Logout and onboard2

--- app/src/main/java/com/noor/homework2_groupb/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/noor/homework2_groupb/view/profile/ProfileFragment.kt
@@ -1,6 +1,21 @@
 package com.noor.homework2_groupb.view.profile
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.noor.homework2_groupb.R
 import com.noor.homework2_groupb.base.BaseFragment
 import com.noor.homework2_groupb.databinding.FragmentProfileBinding
 
-class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate, true)
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate, true){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSignOut.setOnClickListener {
+            Firebase.auth.signOut()
+            findNavController().navigate(R.id.profileFragment_to_loginFragment)
+        }
+    }
+}
